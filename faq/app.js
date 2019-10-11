e94c2d59bfa82d1fe1fc80cb5ef1e656373ad289
kaminskia1 <8168918+kaminskia1@users.noreply.github.com>
2019-10-10 22:52:09 -0400

    Bugfixes

--- faq/app.js
+++ faq/app.js
@@ -1,72 +1,34 @@
+// Grab and list questions
 $.getJSON("data.json", (data)=>
 {
-    data = JSON.parse(data);
-    for (i=0;i<count(data);i++)
-    {
-        faqData.push((data, i)=>
-        {
-            return "<li class='question'><div class='keywords'>" + data[i].keywords + "</div><div class='header'><i class='fas fa-caret-right'></i><div class='word'> " + data[i].question + "</div></div><div class='answer'>" + data[i].answer + "</div></li>";
-        });
-    }
-    faqData = faqData.join();
-    console.log(faqData);
-    //$("ul.holder").html()
-    for(i=0;i<$("ul.holder > li").length;i++)
-    {
-        $("ul.holder > li")[i].classList.add("shown");
-    }
-
-});
-
-
-/* Temp solution for displaying offline, as local getJSON is blocked by CORS
-    var faqData = [],
-    data = [
-            {
-                "question": "Lorem",
-                "answer": "Ipsum",
-                "keyword": [
-                    "Lorem334",
-                    "Ipsum45",
-                    "Dolor65"
-                ]
-            },
-            {
-                "question": "Lorem",
-                "answer": "Ipsum",
-                "keyword": [
-                    "Lorem44",
-                    "Ipsum92",
-                    "Dolor12"
-                ]
-            },
-            {
-                "question": "Lorem",
-                "answer": "Ipsum",
-                "keyword": [
-                    "Lorem12",
-                    "Ipsum23",
-                    "Dolor36"
-                ]
-            }, 
-            {
-                "question": "Lorem2",
-                "answer": "Ipsum2",
-                "keyword": [
-                    "Lorem74",
-                    "Ipsum2",
-                    "Dolor2"
-                ]
-            }
-        ];
-    for (i=0;i<data.length;i++)
-    {
+    var faqData = []; 
+    for (i=0;i<data.length;i++) {
         faqData.push("<li class='question'><div class='keywords'>" + JSON.stringify(data[i].keyword) + "</div><div class='header'><i class='fas fa-caret-right'></i><div class='word'> " + data[i].question + "</div></div><div class='answer'>" + data[i].answer + "</div></li>");
         $("ul.holder").append(faqData[i]);
         $("ul.holder > li")[i].classList.add("shown");
     }
     $("span.output").html($("ul.holder > li.question.shown").length);
-*/
+
+    // Popout / Collapse function
+    $("ul.holder > li.question.shown > div.header").click((a)=>
+    {
+        var ele = a.currentTarget, tri = $(ele).children().first(), ans = $(ele).siblings().last();
+    
+        if (ans.hasClass("visible"))
+        {
+            ans.removeClass("visible");
+            tri.css({
+                transform: "rotate(0deg)",
+            });
+        } else {
+            ans.addClass("visible");
+            tri.css({
+                transform: "rotate(90deg)",
+            });
+        }
+    });
+    
+});
 
 // Search function
 $("input.search").keyup(()=>
@@ -96,6 +58,7 @@ $("input.search").keyup(()=>
 
 });
 
+// Check keyword to search
 function kwCheck(ele, kw, filter)
 {
     var t = false;
@@ -107,23 +70,3 @@ function kwCheck(ele, kw, filter)
     }
     return t;
 }
-
-// Popout / Collapse function
-$("ul.holder > li.question.shown > div.header").click((a)=>
-{
-    var ele = a.currentTarget, tri = $(ele).children().first(), ans = $(ele).siblings().last();
-
-    if (ans.hasClass("visible"))
-    {
-        ans.removeClass("visible");
-        tri.css({
-            transform: "rotate(0deg)",
-        });
-    } else {
-        ans.addClass("visible");
-        tri.css({
-            transform: "rotate(90deg)",
-        });
-    }
-
-});
